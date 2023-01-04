c9083518f8008920e05ecc6956856cdf4e32eea2
jake <th3hopperr@gmail.com>
2023-01-04 16:07:12 -0500

    yessir

--- db/activities.js
+++ db/activities.js
@@ -1,24 +1,77 @@
+/* eslint-disable no-unexpected-multiline */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-useless-catch */
 const client = require('./client');
 
 // database functions
 async function createActivity({ name, description }) {
-  // return the new activity
+  // return the new activity 
   try {
-    const { rows: [activites] } = await client.query(`
-    SELECT * FROM activites
-    `, [activites])
+  const { rows: [activities] } = await client.query 
+   (`
+   INSERT INTO activities ("name", "description")
+   VALUES ($1, $2)
+   RETURNING *;
+   `, [name, description])
+ 
+   return activities;
+ 
   } catch (error) {
-    throw error;
+    throw error
   }
-}
+   
+ }
 
 async function getAllActivities() {
   // select and return an array of all activities
+  try { 
+    const { rows: activitiesName } = await client.query(`
+      SELECT name
+      FROM activities
+    `);
+
+    const activities = await Promise.all(activitiesName.map(
+      activities => getActivityById (activity.name)
+    ));
+
+    return activities;
+
+  } catch (error) {
+    throw (error)
+  }
 }
 
-async function getActivityById(id) { }
+async function getActivityById(id) { 
+  try {
+    const { rows: [ activity ]  } = await client.query(`
+      SELECT *
+      FROM activities
+      WHERE id=$1;
+    `, [id]);
 
-async function getActivityByName(name) { }
+    return activity;
+    
+  } catch (error) {
+    throw error
+  }
+  
+ }
+
+async function getActivityByName(name) { 
+  // eslint-disable-next-line no-useless-catch
+  try {
+    const { rows: [ activities ] } = await client.query( `
+      SELECT name 
+      FROM activities
+      WHERE id=${ name }
+    `)
+    return activities;
+
+  } catch (error) {
+    throw error
+  }
+}
 
 async function attachActivitiesToRoutines(routines) {
   // select and return an array of all activities
@@ -38,3 +91,14 @@ module.exports = {
   createActivity,
   updateActivity,
 };
+
+
+
+
+//   try {
+//     const { rows: [] } = await client.query(`
+//     SELECT * FROM 
+//     `, [])
+//   } catch (error) {
+//     throw error;
+//   }
--- db/users.js
+++ db/users.js
@@ -88,6 +88,5 @@ module.exports = {
   getUserById,
   getUserByUsername,
 }
-const girl = women;
-// ya boi
+
 // by thursday morning if you arent on apis then let sean know.
